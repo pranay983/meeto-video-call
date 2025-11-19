2fcffc8988e0abdff288a68dc41e4cbcecfcb4f6
pranay983 <123645563+pranay983@users.noreply.github.com>
2025-11-19 12:59:45 +0530

    Fix import path for useThemeStore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import  { Toaster } from "react-hot-toast";
 import PageLoader from "./components/PageLoader.jsx";
 import useAuthUser from "./hooks/useAuthUser.js";
 import Layout from "./components/Layout.jsx";
-import { useThemeStore } from "./store/useThemestore.js";
+import { useThemeStore } from "./store/useThemeStore.js";
 
 
 const App = () => {
