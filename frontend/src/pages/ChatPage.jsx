53d513a2ce159a6cf0f8d247eb784c9ce54733ee
pranay983 <123645563+pranay983@users.noreply.github.com>
2025-11-19 13:09:06 +0530

    Fix import case for ChatLoader component

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -15,7 +15,7 @@ import {
 } from "stream-chat-react";
 import { StreamChat } from "stream-chat";
 import toast from "react-hot-toast";
-import ChatLoader from "../components/chatLoader";
+import ChatLoader from "../components/ChatLoader";
 import CallButton from "../components/CallButton";
 
 const MEETO_API_KEY = import.meta.env.VITE_MEETO_API_KEY
